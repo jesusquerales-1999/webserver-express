176b1ee48df4170aae1d143b919a6b42285dabdf
jesus <jesusqueralezz@gmail.com>
2022-07-13 11:18:26 -0400

    inicializando mi proyecto web-server primercommit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const hbs =require('hbs');
+/**Esto se utiliza para servir contenido estatico en la web
+ * Es un midelware.
+ */
+app.use( express.static( __dirname + '/public'));
+
+
+hbs.registerPartials( __dirname + '/views/parciales');
+
+app.set('view engine', 'hbs');
+
+
+
+
+app.get('/',(req,res)=>{
+    res.render('home',{
+        nombre:'Sr Querales',
+        anio: new Date().getFullYear()
+    });
+});
+
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        anio: new Date().getFullYear()
+    });
+});
+
+
+app.listen(3000,()=>{
+    console.log('App corriendo en el puerto',3000);
+})
